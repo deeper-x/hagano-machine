7c5eb6e05551bfed37c06e767724b311a3010927
deeper-x <albertodeprezzo@gmail.com>
2019-03-11 12:33:11 +0100

    removing unuseful import

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"github.com/deeper-x/hagano-machine/pkgs/httpRegister"
 	"github.com/deeper-x/hagano-machine/pkgs/httpServer"
 	"github.com/deeper-x/hagano-machine/pkgs/strFormatters"
-
-	_ "github.com/lib/pq"
 )
 
 func main() {
