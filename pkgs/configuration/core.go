98f95a6ba4a9cd634ed583b9b452ac5fd84f3202
deeper-x <albertodeprezzo@gmail.com>
2019-03-14 01:04:18 +0100

    url with parameter

---
+++ pkgs/configuration/core.go
@@ -0,0 +1,16 @@
+package configuration
+
+// Port ... todo description
+const Port = 8080
+
+// Db ... todo description
+const Db = "*********"
+
+// User ...  todo description
+const User = "**********"
+
+// Host ...  todo description
+const Host = "**********"
+
+// Password ...  todo description
+const Password = "**********"
